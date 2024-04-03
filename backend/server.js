983f4cb91d761cd409dd9cfb5395e9e613c41861
Joonas Salminen <joonas.ej.salminen@gmail.com>
2024-04-03 18:43:07 +0300

    Remove build, modify server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger');
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`);
+const port = config.PORT || 3001;
+server.listen(port, () => {
+    logger.info(`Server running on port ${port}`);
 });
