4868034e0047294875d068447c1cbbde7a18ec36
saljoo <121517674+saljoo@users.noreply.github.com>
2024-04-03 18:50:57 +0300

    Dev to master (#3)
    * Backend preparations for db and api questions
    
    * UI build
    
    * Remove build, modify server.js

--- backend/controllers/dbController.js
+++ backend/controllers/dbController.js
@@ -1,9 +1,9 @@
 const DBController = require('express').Router();
 const Question = require('../models/question');
 
-DBController.get('/', async (req, res) => {
-    const questions = await Question.find({});
-    res.json(questions);
-});
+// DBController.get('/', async (req, res) => {
+//     const questions = await Question.find({});
+//     res.json(questions);
+// });
 
 module.exports = DBController;
