04d6afdff41cc231f9bb0d2e3c9e9a2816ad93d1
saljoo <121517674+saljoo@users.noreply.github.com>
2024-04-03 18:10:06 +0300

    Dev (#2)
    * Backend preparations for db and api questions
    
    * UI build

---
+++ backend/utils/middleware.js
@@ -0,0 +1,19 @@
+const logger = require('./logger');
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'Unknown endpoint' });
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+    next(error);
+}
+
+module.exports = {
+    unknownEndpoint,
+    errorHandler
+}
