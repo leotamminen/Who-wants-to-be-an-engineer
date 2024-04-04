d14e95617486ab18b37c5475def92fc4f9b473d3
Leo Tamminen <lettam@utu.fi>
2024-04-04 21:01:37 +0300

    output test

--- backend/controllers/dbController.js
+++ backend/controllers/dbController.js
@@ -1,10 +1,10 @@
-const dbController = require('express').Router();
-const Question = require('../models/question');
+const dbController = require("express").Router();
+const Question = require("../models/question");
 
 // using questionsDB.questionscollections
-dbController.get('/', async (req, res) => {
-    const questions = await Question.find({});
-    res.json(questions);
+dbController.get("/", async (req, res) => {
+  const questions = await Question.find({});
+  res.json(questions);
 });
 
 module.exports = dbController;
