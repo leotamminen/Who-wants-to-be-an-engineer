425f92c7d1f935a3dabf7a95a860d9faea5ede0a
Joonas Salminen <joonas.ej.salminen@gmail.com>
2024-04-05 13:10:03 +0300

    Change back to working version

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -6,7 +6,7 @@ APIController.get("/", async (req, res) => {
   try {
     // Call questionGenerator method from QuestionGenerator class
     const result = await QuestionGenerator.questionGenerator();
-    res.send(result.candidates.output);
+    res.json(result);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
