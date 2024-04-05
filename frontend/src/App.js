c4076986e10b1bc657e816343f8ea4de4d4c3283
Sodejaa <soderstrom.jaani@gmail.com>
2024-04-05 20:25:54 +0300

    frontend styling changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,6 @@ function App() {
   useEffect(() => {
     // only start tracking after player got through first question
     questionNumber > 1 &&
-      // Find the right amount in prizeSums and update players earned money
       setEarnedMoney(
         prizeSums.find((item) => item.id === questionNumber - 1).amount
       );
@@ -30,56 +29,53 @@ function App() {
     setIsMillionaire(true);
   };
 
-  // If name is null, render the Start screen.
+  // Only render the game content if the name is provided
   return (
     <div className="App">
-      {!name ? (
-        <Start setName={setName} setTimeOut={setTimeOut} />
-      ) : (
-        <div className="game-container">
-          <div className="game">
-            {timeOut ? (
-              <GameOver className="game-over" earnedMoney={earnedMoney} />
-            ) : isMillionaire ? (
-              <GameWinner className="game-over" />
-            ) : (
-              <>
-                <div className="timer">
-                  <Timer
-                    setTimeOut={setTimeOut}
-                    questionNumber={questionNumber}
-                    answersLocked={answersLocked}
-                  />
-                </div>
-                <div>
-                  <Quiz
-                    questions={questions}
-                    questionNumber={questionNumber}
-                    setQuestionNumber={setQuestionNumber}
-                    setTimeOut={setTimeOut}
-                    setAnswersLocked={setAnswersLocked}
-                    handleBecomeMillionaire={handleBecomeMillionaire}
-                  />
-                </div>
-              </>
-            )}
+      {name ? (
+        <>
+          <div className="game-container">
+            <div className="timer-container">
+              <div className="timer">
+                <Timer
+                  setTimeOut={setTimeOut}
+                  questionNumber={questionNumber}
+                  answersLocked={answersLocked}
+                />
+              </div>
+            </div>
+            <div className="game">
+              {timeOut ? (
+                <GameOver className="game-over" earnedMoney={earnedMoney} />
+              ) : isMillionaire ? (
+                <GameWinner className="game-over" />
+              ) : (
+                <Quiz
+                  questions={questions}
+                  questionNumber={questionNumber}
+                  setQuestionNumber={setQuestionNumber}
+                  setTimeOut={setTimeOut}
+                  setAnswersLocked={setAnswersLocked}
+                  handleBecomeMillionaire={handleBecomeMillionaire}
+                />
+              )}
+            </div>
           </div>
-
           <div className="money-container">
             <ul className="money-list">
               {prizeSums.map((item) => (
                 <li
                   key={item.id}
-                  className={
-                    questionNumber === item.id ? "item active" : "item"
-                  }
+                  className={questionNumber === item.id ? "item active" : "item"}
                 >
                   <h5 className="amount">{item.amount}</h5>
                 </li>
               ))}
             </ul>
           </div>
-        </div>
+        </>
+      ) : (
+        <Start setName={setName} setTimeOut={setTimeOut} />
       )}
     </div>
   );
